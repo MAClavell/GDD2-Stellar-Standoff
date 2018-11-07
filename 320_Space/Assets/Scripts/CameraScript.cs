82f12f6f338dd98d8f2ff9d0ebe408ee58e77058
Paul Avallone (RIT Student) <paa9307@ad.rit.edu>
2018-11-07 09:51:42 -0500

    CameraScript and scene

---
+++ 320_Space/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+
+    enum CameraState { Start, Base, Full ,End };
+    CameraState currState;
+
+	// Use this for initialization
+	void Start () {
+        currState = CameraState.Start;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(currState == CameraState.Start)
+        {
+
+        }
+        else if (currState == CameraState.Base)
+        {
+            //Camera.main.orthographicSize = Mathf.MoveTowards(Camera.main.orthographicSize,);
+        }
+        else if (currState == CameraState.Full)
+        {
+
+        }
+        else if (currState == CameraState.End)
+        {
+
+        }
+
+    }
+}
