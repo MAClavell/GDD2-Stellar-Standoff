2548c8b0ecda9e14c69ddbe172e75912af9beb38
Michael Clavell (RIT Student) <mac9406@ad.rit.edu>
2018-11-06 19:05:07 -0500

    Unity project and game state control

---
+++ 320_Space/Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameState { Begin, Paused, Playing, End }
+enum RoundState { Begin, TurnOrder, PlayChoices, End }
+enum TurnState { Begin, Choosing, End }
+
+public class GameManager : Singleton<GameManager> {
+
+    /// <summary>
+    /// The current GameState
+    /// </summary>
+    public GameState State { get; set; }
+
+    public List<PlayerStation> players { get; set; }
+
+    //Private Fields
+    RoundState roundState;
+    short currPlayer;
+
+    // Called before start
+    void Awake () {
+        State = GameState.Begin;
+        roundState = RoundState.Begin;
+        players = new List<PlayerStation>();
+        currPlayer = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        switch (State)
+        {
+            //Game is starting
+            case GameState.Begin:
+                State = GameState.Playing;
+                break;
+
+            //Game is paused
+            case GameState.Paused:
+
+                break;
+
+            //Playing the game
+            case GameState.Playing:
+                switch (roundState)
+                {
+                    //
+                    case RoundState.Begin:
+                        currPlayer = 0;
+                        foreach(PlayerStation player in players)
+                        {
+                            player.ActionChosen = false;
+                        }
+                        break;
+
+                    //Players are taking their turns
+                    case RoundState.TurnOrder:
+
+                        //Should never be less than 2 players
+                        if (players.Count < 2)
+                        {
+                            Debug.LogError("Should never be less than 2 players!");
+                            return;
+                        }
+
+                        //End the turn order if we are past the max players
+                        if(currPlayer == players.Count)
+                        {
+                            roundState = RoundState.PlayChoices;
+                            return;
+                        }
+
+                        //Player chooses their action
+                        if(players[currPlayer].IsAlive && !players[currPlayer].ActionChosen)
+                        {
+                            players[currPlayer].ChooseAction();
+                        }
+                        break;
+
+                    //Play all the choices out
+                    case RoundState.PlayChoices:
+                        foreach (PlayerStation player in players)
+                        {
+                            player.PerformAction();
+                        }
+                        roundState = RoundState.End;
+                        break;
+
+                    //End the round
+                    case RoundState.End:
+                        roundState = RoundState.Begin;
+                        break;
+                }
+                break;
+
+            //Ending the game
+            case GameState.End:
+
+                break;
+        }
+    }
+}
---
+++ 320_Space/Assets/Scripts/PlayerStation.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStation : MonoBehaviour {
+
+    /// <summary>
+    /// Whether the station is alive
+    /// </summary>
+    public bool IsAlive { get { return health > 0; } }
+
+    /// <summary>
+    /// Whether the station has chosen their action or not
+    /// </summary>
+    public bool ActionChosen { get; set; }
+
+    //Private fields
+    short health;
+    short resources;
+
+	// Called before start
+	void Awake () {
+        health = 3;
+        resources = 0;
+        ActionChosen = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    /// <summary>
+    /// Logic for the player choosing their action
+    /// </summary>
+    public void ChooseAction()
+    {
+
+    }
+
+    /// <summary>
+    /// Logic for the player performing their action
+    /// </summary>
+    public void PerformAction()
+    {
+        
+    }
+}
