8b72825556355c51b907a02b3a855d8f782fac06
Sam <sxf5282@rit.edu>
2018-11-10 13:01:07 -0500

    Test Missiles Rotate
    Up vector of missiles responds to arc. Discovered a problem when you launch to the opposite side of the asteroid; not sure of the best solution

--- 320_Space/Assets/Scripts/TestMissile.cs
+++ 320_Space/Assets/Scripts/TestMissile.cs
@@ -25,25 +25,38 @@ public class TestMissile : MonoBehaviour {
         interval = 2.5f;
         maxHeight = 1.0f;
         originIndex = 0;
-        destinationIndex = 1;
+        destinationIndex = 4;
     }
 	
 	//Update is called once per frame
 	void Update ()
     {
 
-
+        //Update Interpolation data
         timer += Time.deltaTime;
         float fracComplete = timer / interval;
         Vector3 slerp = Vector3.Slerp(stations[originIndex].Direction, stations[destinationIndex].Direction, fracComplete);
-        
+        //slerp.z = 0;
+        //slerp = new Vector3(slerp.x, slerp.y, 0);
+        //Quaternion qSlerp = Quaternion.Slerp(Quaternion.Euler(stations[originIndex].Direction), Quaternion.Euler(stations[destinationIndex].Direction), fracComplete);
+        //qSlerp = Quaternion.Euler(new Vector3(0f, qSlerp.eulerAngles.y, 0f));
+
         float arc = Mathf.Sin(fracComplete * Mathf.PI);
         transform.position = (asteroid.radius + (arc * maxHeight)) * slerp.normalized;
 
-        if(fracComplete >= 1.0f)
+        //Update Rotation
+        float angle = 90.0f;
+        if(Vector3.SignedAngle(stations[originIndex].Direction, stations[destinationIndex].Direction, Vector3.forward) > 0)
+                angle = -90.0f;
+        transform.up = Quaternion.AngleAxis(angle, Vector3.forward) * slerp.normalized;
+
+
+        //Arrive at destination of interpolation
+        if (fracComplete >= 1.0f)
         {
             timer = 0;
 
+            //Start next interpolation
             originIndex++;
             if (originIndex >= stations.Count) originIndex = 0;
             destinationIndex++;
